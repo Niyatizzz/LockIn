1230e9c48447d29832e16973dd350d0258b0db31
Niyatizzz <niyatikolukula@gmail.com>
2025-06-30 22:16:34 +0530

    landing page

---
+++ client/lockin/src/utils/data.js
@@ -0,0 +1,32 @@
+export const APP_FEATURES = [
+    {
+        id: "01",
+        title: "Tailored Just for You",
+        description:
+            "Get interview questions and model answers based on your role, experience, and specific focus areas"
+    },
+    {
+        id: "02",
+        title: "Learn at Your Own Pace",
+        description:
+            "Expand answer only when you're ready. Dive deeper into any concept instantly with AI-powered detailed"
+    },
+    {
+        id: "03",
+        title: "Capture Your Insights",
+        description:
+            "Add personal notes to any question and pin important ones to the top - making your learning more organized"
+    },
+    {
+        id: "04",
+        title: "Tailored Just for You",
+        description:
+            "Get interview questions and model answers based on your role, experience, and specific focus areas"
+    },
+    {
+        id: "01",
+        title: "Tailored Just for You",
+        description:
+            "Get interview questions and model answers based on your role, experience, and specific focus areas"
+    },
+]
